1dadf1bef07e9c8a0654f3b5b8ced657be879748
selahattinasn <selahattinasn@gmail.com>
2025-03-05 20:31:25 +0100

    updated

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,18 +1,23 @@
-from flask import Flask 
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 
-
+def head():
+     return "Hello World"
 @app.route('/secondpage')
 
+def second():
+     return "This is second page!"
+@app.route('/thirdpage')
 
-@app.route('/third')
-
-
+def third():
+     return "This is thirth page!"
 @app.route('/fourth/<string:id>')
-
+def fourt(id):
+     return f'id of this page is {id}!'
 
 if __name__ == '__main__':
 
-     # app.run(debug=True)
+     #app.run(debug=True, port=3000)
+     #app.run(debug=True)
      app.run(host= '0.0.0.0', port=80)
