7fc4cec1ac3691e22853ffca27914aae4a22d210
selahattinasn <selahattinasn@gmail.com>
2025-03-06 13:46:59 +0100

    updated

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -19,5 +19,5 @@ def fourt(id):
 if __name__ == '__main__':
 
      #app.run(debug=True, port=3000)
-     #app.run(debug=True)
-     app.run(host= '0.0.0.0', port=80)
+     app.run(debug=True)
+     #app.run(host= '0.0.0.0', port=80)
